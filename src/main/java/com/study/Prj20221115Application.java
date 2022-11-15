201738ca5c4c26806a7a433fd9d48fae4a2e2825
rarion <110441862+rarion@users.noreply.github.com>
2022-11-15 14:30:22 +0900

    first commit

---
+++ src/main/java/com/study/Prj20221115Application.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj20221115Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj20221115Application.class, args);
+	}
+
+}
